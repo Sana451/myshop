ba25a0c130b4cab6bcd38a3e1ad44e785d6f7cb5
sana451 <sana451@mail.ru>
2023-02-09 06:29:44 +0300

    Добавлены элементы локализации.

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Order
 
+from localflavor.ru.forms import RUPostalCodeField
+from localflavor.us.forms import USZipCodeField
+
 
 class OrderCreateForm(forms.ModelForm):
+    postal_code = RUPostalCodeField()
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
